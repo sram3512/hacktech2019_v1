48f916659e9e7a676133a2ec49bcb0dddab4b82e
ChaitanyAmbegaonkar <ambegaon@usc.edu>
2019-03-02 10:24:27 -0800

    Initial Commit

---
+++ chatbot_backend.py
@@ -0,0 +1,233 @@
+# things we need for NLP
+import nltk
+from nltk.stem.lancaster import LancasterStemmer
+stemmer = LancasterStemmer()
+
+# things we need for Tensorflow
+import numpy as np
+import tflearn
+import tensorflow as tf
+import random
+
+# import our chat-bot intents file
+import json
+# with open('intents_suicide.json') as json_data:
+#     intents = json.load(json_data)
+#
+# intents_secondary = ""
+with open('intents.json') as json_data:
+    intents = json.load(json_data)
+
+# for i in intents_secondary['intents']:
+#     intents['intents'].append(i)
+
+nltk.download('punkt')
+words = []
+classes = []
+documents = []
+ignore_words = ['?']
+# loop through each sentence in our intents patterns
+for intent in intents['intents']:
+    for pattern in intent['patterns']:
+        # tokenize each word in the sentence
+        w = nltk.word_tokenize(pattern)
+        # add to our words list
+        words.extend(w)
+        # add to documents in our corpus
+        documents.append((w, intent['tag']))
+        # add to our classes list
+        if intent['tag'] not in classes:
+            classes.append(intent['tag'])
+
+# stem and lower each word and remove duplicates
+words = [stemmer.stem(w.lower()) for w in words if w not in ignore_words]
+words = sorted(list(set(words)))
+
+# remove duplicates
+classes = sorted(list(set(classes)))
+
+print (len(documents), "documents")
+print (len(classes), "classes", classes)
+print (len(words), "unique stemmed words", words)
+
+
+
+
+
+
+# create our training data
+training = []
+output = []
+# create an empty array for our output
+output_empty = [0] * len(classes)
+
+# training set, bag of words for each sentence
+for doc in documents:
+    # initialize our bag of words
+    bag = []
+    # list of tokenized words for the pattern
+    pattern_words = doc[0]
+    # stem each word
+    pattern_words = [stemmer.stem(word.lower()) for word in pattern_words]
+    # create our bag of words array
+    for w in words:
+        bag.append(1) if w in pattern_words else bag.append(0)
+
+    # output is a '0' for each tag and '1' for current tag
+    output_row = list(output_empty)
+    output_row[classes.index(doc[1])] = 1
+
+    training.append([bag, output_row])
+
+# shuffle our features and turn into np.array
+random.shuffle(training)
+training = np.array(training)
+
+# create train and test lists
+train_x = list(training[:,0])
+train_y = list(training[:,1])
+
+
+
+
+
+
+
+# reset underlying graph data
+tf.reset_default_graph()
+# Build neural network
+net = tflearn.input_data(shape=[None, len(train_x[0])])
+net = tflearn.fully_connected(net, 8)
+net = tflearn.fully_connected(net, 8)
+net = tflearn.fully_connected(net, len(train_y[0]), activation='softmax')
+net = tflearn.regression(net)
+
+# Define model and setup tensorboard
+model = tflearn.DNN(net, tensorboard_dir='tflearn_logs')
+# Start training (apply gradient descent algorithm)
+model.fit(train_x, train_y, n_epoch=1000, batch_size=8, show_metric=True)
+model.save('model.tflearn')
+
+
+
+def clean_up_sentence(sentence):
+    # tokenize the pattern
+    sentence_words = nltk.word_tokenize(sentence)
+    # stem each word
+    sentence_words = [stemmer.stem(word.lower()) for word in sentence_words]
+    return sentence_words
+
+# return bag of words array: 0 or 1 for each word in the bag that exists in the sentence
+def bow(sentence, words, show_details=False):
+    # tokenize the pattern
+    sentence_words = clean_up_sentence(sentence)
+    # bag of words
+    bag = [0]*len(words)
+    for s in sentence_words:
+        for i,w in enumerate(words):
+            if w == s:
+                bag[i] = 1
+                if show_details:
+                    print ("found in bag: %s" % w)
+
+    return(np.array(bag))
+
+
+p = bow("is your shop open today?", words)
+print (p)
+print (classes)
+
+# print(model.predict([p]))
+#
+#
+# # save all of our data structures
+# import pickle
+# pickle.dump( {'words':words, 'classes':classes, 'train_x':train_x, 'train_y':train_y}, open( "training_data", "wb" ) )
+
+
+# load our saved model
+model.load('./model.tflearn')
+
+
+# create a data structure to hold user context
+context = {}
+
+# ERROR_THRESHOLD = 0.15
+def classify(sentence,ERROR_THRESHOLD):
+    # generate probabilities from the model
+
+    results = model.predict([bow(sentence, words)])[0]
+    # filter out predictions below a threshold
+    results = [[i,r] for i,r in enumerate(results) if r>ERROR_THRESHOLD]
+    print(results)
+    # sort by strength of probability
+    results.sort(key=lambda x: x[1], reverse=True)
+    return_list = []
+    for r in results:
+        return_list.append((classes[r[0]], r[1]))
+    # return tuple of intent and probability
+    return return_list
+
+def response(sentence,con):
+    results = ()
+    ERROR_THRESHOLD = 0.15
+    while not len(results) :
+        results = classify(sentence,ERROR_THRESHOLD)
+        ERROR_THRESHOLD = ERROR_THRESHOLD - 0.02
+    # print("results list : ", results)
+    # if we have a classification then find the matching intent tag
+    if results:
+        # loop as long as there are matches to process
+        for result in results:
+            for i in intents['intents']:
+                print("result[0] = ", result[0])
+                if i['tag'] == result[0]:
+                    print(i)
+                    if i['context_filter'] == con:
+                        print(random.choice(i['responses']))
+                        con = int(i['context_set'])
+                        return con;
+        for result in results:
+            for i in intents['intents']:
+                if i['tag'] == result[0]:
+                    print(random.choice(i['responses']))
+                    con = int(i['context_set'])
+                    print("con = ", con)
+                    return con;
+
+    return con;
+
+# classify('is your shop open today?')
+#
+# response('is your shop open today?')
+# response('do you take cash?')
+#
+# response('what kind of mopeds do you rent?')
+# response('Goodbye, see you later')
+# context
+# response('we want to rent a moped')
+# context
+# response('today')
+#
+# classify('today')
+#
+# # clear context
+# response("Hi there!", show_details=True)
+# response('today')
+# classify('today')
+# response("thanks, your great")
+user_input = ""
+while "done" not in user_input.lower():
+    user_input = ""
+    con = 0
+    print("\n\nHello!")
+    while(con != -1):
+        user_input = input("")
+        if "done" in user_input.lower():
+            break
+        con = response(user_input,con)
+
+# Continue till a certain context is reached
+# All the ending context can be same
+# We need to keep on calling the classify function till we don't have a correct response
+#
